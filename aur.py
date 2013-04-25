5e67af3e16cf5185f443dc9fa5f7152ebe8f74e6
Tyler Corkill <TylerCorkill@gmail.com>
2013-04-25 16:51:50 -0700

    modified aur script flags

--- aur.py
+++ aur.py
@@ -35,38 +35,33 @@ def sync(package):
 length = len(package)
 if length > 0:
 	if package[0] == "-" and length > 1:
-		# U for Upgrade
-		if package[1] == "U":
-			if length < 3:
-				print "Error: unimplimented"
-			# a for all
-			elif package[2] == "a":
-				forLine = re.compile("(?<=<h2>Package Details: )[\w\s\.\-]+")
-				forLocPac = re.compile("[a-zA-Z\-]+")
-				# forRmPac = re.compile("(?<=<h2>Package Details: )[a-zA-Z \-]+")
-				process = Popen("pacman -Qm", shell=True, stdout=PIPE)
-				outOfDate = []
-				while True:
-					line = process.stdout.readline()
-					if line == "":
-						break
-					pac = forLocPac.search(line).group(0)
-					url = "%s/%s/" % (baseURL, pac)
-					# print url
-					urlLines = []
-					for urlLine in urlopen(url).readlines():
-						urlLines.append(urlLine.strip())
-					for entry in urlLines:
-						detail = forLine.search(entry)
-						if detail != None:
-							# print detail.group(0), line[:-1]
-							if detail.group(0) != line[:-1]:
-								print pac
-								outOfDate.append(pac)
-				for pac in outOfDate:
-					sync(pac)
+		# u for upgrade
+		if package[1] == "u":
+			forLine = re.compile("(?<=<h2>Package Details: )[\w\s\.\-]+")
+			forPackage = re.compile("[a-zA-Z\-]+")
+			process = Popen("pacman -Qm", shell=True, stdout=PIPE)
+			outOfDate = []
+			while True:
+				line = process.stdout.readline()
+				if line == "":
+					break
+				pac = forPackage.search(line).group(0)
+				url = "%s/%s/" % (baseURL, pac)
+				# print url
+				urlLines = []
+				for urlLine in urlopen(url).readlines():
+					urlLines.append(urlLine.strip())
+				for entry in urlLines:
+					detail = forLine.search(entry)
+					if detail != None:
+						# print detail.group(0), line[:-1]
+						if detail.group(0) != line[:-1]:
+							print pac
+							outOfDate.append(pac)
+			for pac in outOfDate:
+				sync(pac)
 		else:
-			print "Error: unknown command: %s" % package
+			print "Error: unknown flag: %s" % package
 	else:
 		sync(package)
 else:
