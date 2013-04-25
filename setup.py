ad2b56ab111e99e3d19af61e1b643d03ab3b5f20
TylerCorkill <TylerCorkill@gmail.com>
2013-04-25 16:01:21 -0700

    Added setup script

--- setup.py
+++ setup.py
@@ -6,3 +6,6 @@ files = [".bashrc",
 for f in files:
 	os.system("rm ~/%s" % f)
 	os.system("ln ~/.custom/%s ~/%s" % (f,f))
+print "Updated bash files"
+os.system("rm -r ~/.custom/aur")
+os.system("mkdir ~/.custom/aur")
