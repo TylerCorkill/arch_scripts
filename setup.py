1c22af46cd482ddb19215bd6094d095255ec9d91
Tyler Corkill <tsx525@gmail.com>
2013-06-07 19:08:04 -0700

    Fixed minor bug

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ for f in ['.bashrc',
               'ln ~/.custom/{0} ~/{0}'.format(f))
 
 os.system('sudo rm /etc/bash.bashrc;'
-          'sudo ln ~/.custom/bash.bashrc /etc/bash.bashrc')
+          'sudo cat ~/.custom/bash.bashrc > /etc/bash.bashrc')
 
 print "Updated bash files"
