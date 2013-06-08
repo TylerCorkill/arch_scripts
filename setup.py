d02237fc6486e24120e2e5f1659f91ba72ae4d14
Tyler Corkill <tsx525@gmail.com>
2013-06-07 18:52:30 -0700

    Changing bash modifications file

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@ for f in ['.bashrc',
     os.system('rm ~/{0};'
               'ln ~/.custom/{0} ~/{0}'.format(f))
 
+os.system('sudo rm /etc/bash.bashrc;'
+          'sudo ln ~/.custom/bash.bashrc /etc/bash.bashrc')
+
 print "Updated bash files"
