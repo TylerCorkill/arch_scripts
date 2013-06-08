9b5b60136586e78f9c69102701e6c17926488624
Tyler Corkill <tsx525@gmail.com>
2013-06-07 19:36:33 -0700

    Moved bash files

--- .bashrc
+++
@@ -1,8 +0,0 @@
-#
-# ~/.bashrc
-#
-
-# If not running interactively, don't do anything
-[[ $- != *i* ]] && return
-
-[ -r /usr/share/bash-completion/bash_completion   ] && . /usr/share/bash-completion/bash_completion
--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 import os
 
-for f in ['.bashrc',
-          '.bash_profile',
-          '.bash_logout']:
-    os.system('rm ~/{0};'
-              'ln ~/.custom/{0} ~/{0}'.format(f))
-
 os.system("sudo sh -c 'cat bash.bashrc > /etc/bash.bashrc'")
 
 print "Updated bash files"
